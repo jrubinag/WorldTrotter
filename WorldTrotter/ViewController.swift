d229ab02156dc6da14941f968540c738375ee329
jrubinag <jonathan_hrg@hotmail.com>
2020-03-18 23:21:45 -0500

    Iitial World Trotter setup - Chapter 4

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Jonathan Rubina on 2/1/20.
+//  Copyright © 2020 Jonathan Rubina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
