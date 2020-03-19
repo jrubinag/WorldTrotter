5d34c021d4f70d121c2ef472d5802ada83cf004a
jrubinag <jonathan_hrg@hotmail.com>
2020-03-19 01:53:18 -0500

    Add new Controller

---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Jonathan Rubina on 3/19/20.
+//  Copyright © 2020 Jonathan Rubina. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("MapViewController loaded its view.")
+    }
+}
